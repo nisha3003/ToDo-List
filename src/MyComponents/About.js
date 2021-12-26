c7d278572630b6ad5ee4ec287b1a05068f15469a
nisha3003 <51108048+nisha3003@users.noreply.github.com>
2021-12-26 08:32:54 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt nisi similique nobis tempore minima blanditiis facilis quaerat at, provident asperiores dolorum enim sapiente eos nihil earum nam natus voluptate molestias?</p>
+        </div>
+    )
+}
